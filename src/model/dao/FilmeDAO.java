1b31cd071de822e5638edaf8f57aa39a2879ba7b
Raquel Brombilla <raquelbrombilla@hotmail.com>
2020-11-30 16:53:31 -0300

    Listar filmes

--- src/model/dao/FilmeDAO.java
+++ src/model/dao/FilmeDAO.java
@@ -2,7 +2,10 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -33,6 +36,40 @@ public class FilmeDAO {
 		} finally {
 			ConnectionFactory.closeConnection(con, stmt);
 		}
+	}
+	
+	public List<Filme> read(){
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<Filme> filmes = new ArrayList<>(); 
+		
+		try {
+			stmt = con.prepareStatement("SELECT * FROM filme;");
+			rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				Filme f = new Filme();
+				f.setIdFilme(rs.getInt("idFilme"));
+				f.setTitulo(rs.getString("titulo"));
+				f.setTempo(rs.getInt("tempo"));
+				f.setSinopse(rs.getString("sinopse"));
+				f.setCategoria(rs.getString("categoria"));
+				f.setImagem3d(rs.getBoolean("imagem3d"));
+				f.setDublado(rs.getBoolean("dublado"));
+				
+				filmes.add(f);
+			}
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao buscar as informações do BD: " + e);
+			e.printStackTrace();
+		} finally {
+			ConnectionFactory.closeConnection(con, stmt, rs);
+		}
+		
+		return filmes;
+		
 	}
 	
 }
---
+++ src/view/JFListarFilmes.java
@@ -0,0 +1,108 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import model.bean.Filme;
+import model.dao.FilmeDAO;
+
+import javax.swing.JButton;
+
+public class JFListarFilmes extends JFrame {
+
+	private JPanel contentPane;
+	private JTable jtFilmes;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFListarFilmes frame = new JFListarFilmes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFListarFilmes() {
+		setTitle("Listar Filmes");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 625, 432);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblListarFilmes = new JLabel("Listar Filmes");
+		lblListarFilmes.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblListarFilmes.setBounds(10, 24, 158, 21);
+		contentPane.add(lblListarFilmes);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 66, 589, 242);
+		contentPane.add(scrollPane);
+		
+		jtFilmes = new JTable();
+		jtFilmes.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"idFilme", "T\u00EDtulo", "Categoria", "Tempo"
+			}
+		));
+		scrollPane.setViewportView(jtFilmes);
+		
+		JButton btnCadastrar = new JButton("Cadastrar Filme");
+		btnCadastrar.setBounds(10, 338, 130, 23);
+		contentPane.add(btnCadastrar);
+		
+		JButton btnAlterar = new JButton("Alterar Filme");
+		btnAlterar.setBounds(163, 338, 118, 23);
+		contentPane.add(btnAlterar);
+		
+		JButton btnExcluir = new JButton("Excluir Filme");
+		btnExcluir.setBounds(301, 338, 130, 23);
+		contentPane.add(btnExcluir);
+		
+		readJTable();
+	}
+	
+	public void readJTable() {
+		DefaultTableModel modelo = (DefaultTableModel) jtFilmes.getModel();
+		modelo.setNumRows(0);
+		FilmeDAO fdao = new FilmeDAO();
+		
+		for(Filme f : fdao.read()) {
+			modelo.addRow(new Object[] {
+					f.getIdFilme(),
+					f.getTitulo(),
+					f.getCategoria(),
+					f.getTempo()
+					
+			});	
+				
+		}
+	}
+
+}
